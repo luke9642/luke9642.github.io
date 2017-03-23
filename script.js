e89d4b6890b2a6ecf6ba9f332abb014f1bf93655
luke9642 <luke.kowalski9642@gmail.com>
2017-03-23 03:04:53 +0100

    Themes added

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
 	var navTop = $('nav').offset().top;
-	var navHeight = $('nav').outerHeight();
+	var navHeight = $('nav').outerHeight(true);
 	var difference = navHeight;
 	var aboutMeTop = $('#AboutMe').offset().top;
 	var CVTop = $('#CV').offset().top;
 	var ContactTop = $('#Contact').offset().top;
 	var height = -100;
-	
-	var stickyNav = function() {
+	var defaultTheme = "theme1";
+	var currentTheme;
+
+	function stickyNav() {
 		var scrollTop = $(window).scrollTop();
 		if (scrollTop > navTop) {
 			$('#up').css('bottom', '30px');
@@ -19,8 +21,8 @@ $(document).ready(function() {
 			$('header').css("margin-bottom", 0);
 		}
 	};
-	
-	var menuColor = function() {
+
+	function menuColor() {
 		var scrollTop = $(window).scrollTop();
 		if(scrollTop > aboutMeTop + height) {
 			$('nav li.AboutMe').addClass('checked');
@@ -40,11 +42,53 @@ $(document).ready(function() {
 			$('nav li.Contact').removeClass('checked');
 		}
 	};
-	
+
+	function changeTheme(theme) {
+		$('body').removeClass(currentTheme);
+		$('body').addClass(theme);
+		setCookie(theme);
+	}
+
+	function setCookie(theme) {
+		var d = new Date();
+		d.setTime(d.getTime() + (10000*30));
+		var expires = "expires="+ d.toUTCString();
+		document.cookie = "theme=" + theme + ";" + expires + ";";
+		currentTheme = theme;
+	}
+
+	function getCookie() {
+		var decodedCookie = decodeURIComponent(document.cookie);
+	    var ca = decodedCookie.split(';');
+	    for(var i = 0; i < ca.length; ++i) {
+	        var c = ca[i];
+	        while (c.charAt(0) == ' ')
+	            c = c.substring(1);
+	        if (c.indexOf("theme") == 0)
+	            return c.substring(name.length, c.length);
+	    }
+	    return "";
+	}
+
+	function checkCookies() {
+		var theme = getCookie();
+		if(theme != "")
+			changeTheme(theme);
+		else
+			setCookie(defaultTheme);
+	}
+
 	$('#up').click(function() { $('body').stop().animate({scrollTop: 0}, '500', 'swing')});
-	
+
+	$('#theme li').click(function() { changeTheme($(this).attr('id')); });
+
+	$("#AboutMe").load("Content/AboutMe_Pol.html");
+	$("#CV").load("Content/CV_Pol.html");
+	$("#Contact").load("Content/Contact_Pol.html");
+
 	stickyNav();
 	menuColor();
+	checkCookies();
 
 	$(window).scroll(function() {
 		stickyNav();
