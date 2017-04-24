462fadce627c582b69f58df54762d5dfaa6d0ac3
luke9642 <luke.kowalski9642@gmail.com>
2017-04-24 02:59:31 +0200

    Cookies

--- script.js
+++ script.js
@@ -73,14 +73,15 @@ $(document).ready(function() {
 		document.cookie = id + "=" + value + ";" + expires + ";";
 	}
 
-	function getCookie(id) {
+	function getCookie(name) {
+		name = name + "=";
 		var decodedCookie = decodeURIComponent(document.cookie);
 	    var ca = decodedCookie.split(';');
 	    for(var i = 0; i < ca.length; ++i) {
 	        var c = ca[i];
 	        while (c.charAt(0) == ' ')
 	            c = c.substring(1);
-	        if (c.indexOf(id) == 0)
+	        if (c.indexOf(name) == 0)
 	            return c.substring(name.length, c.length);
 	    }
 	    return "";
